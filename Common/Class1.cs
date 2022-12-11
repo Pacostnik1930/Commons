f476279b863f4ee11b21b8255a6a83badc4db4db
Гойхман Петр <pgoyhman@mail.ru>
2022-12-11 18:08:52 +0300

    Common

---
+++ Common/Class1.cs
@@ -0,0 +1,104 @@
+﻿namespace Common;
+
+public static class Helper
+{
+
+    public static int[] CreateRandomArray()  // Создание массива со случайными числами
+    {
+        Random random = new Random();
+        int[] array = new int[10];
+        for (var i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(-10, 11);
+        }
+        return array;
+
+    }
+
+    public static void PrintArray(int[] array)  // Печать массива
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            Console.Write(" ");
+        }
+    }
+
+
+    public static int[] ReverseArray(int[] array)  //Переворот массива
+    {
+        int[] reversedArray = new int[array.Length];
+        for (var i = 0; i < array.Length; i++)
+        {
+            reversedArray[i] = array[array.Length - 1 - i];
+        }
+        return reversedArray;
+    }
+
+
+    public static double InputInt()                      // Метод принимает координаты двух точек
+    {
+        Console.WriteLine("Введите координаты первой точки");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите координаты второй точки");
+        double b = double.Parse(Console.ReadLine());
+        bool isNumber = double.TryParse(Console.ReadLine(), out double number);
+        if (isNumber)
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Введено некорректное значение");
+            return -1;
+        }
+
+    }
+
+    public static double GetDistanceBetweenTwoPoints(double ax, double ay, double bx, double by, double zx, double zy)
+    {
+        double ab = Math.Round(Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2)));
+        return ab;
+    }
+    // Метод для расчета расстояния между двух точек в 3d пространстве
+
+    public static int[] GetCubeTable(int num)       // метод для таблицы кубов
+    {
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++)
+        {
+            array[i] = (int)((Math.Pow(i + 1, 3)));
+        }
+        return array;
+    }
+
+    public static int HowToConstruction(int a, int b)    //Возведение числа в натуральную степень
+
+
+    {
+        int result = a;
+        for (int i = 1; i < b; i++)
+        {
+            result = result * a;
+        }
+        return result;
+    }
+
+    public static int[] CreateArray()   //Создание массива из элементов
+{
+    Console.Write("Введите колличество элементов массива:\t");
+
+int element = int.Parse(Console.ReadLine());
+int[] array = new int[element];
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"\nВведите элемент массива под индексом {i}:\t");
+ array[i]= int.Parse(Console.ReadLine());
+
+}
+Console.WriteLine("[{0}]", string.Join(", ", array));
+return array;
+}
+
+
+}
---
+++ lesson6/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу,которая перевернет одномерный массив
+//(последний элемент будет на первом месте,а первый на последнем)
+
+
+using static Common.Helper;
+
+
+// Console.WriteLine(int.MaxValue);
+// int[] array = CreateRandomArray();
+// PrintArray(array);
+// Console.WriteLine();
+// int[] reversed = ReverseArray(array);
+// PrintArray(reversed);
+
+
+
+//Напишите программу, которая будет преобразовывать десятичное число в двоичное. 45 -> 101101
+//3 -> 11
+//2 -> 10
+
+
